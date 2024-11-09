5f2e6c03af2f88440b900e57a4790ae8189abf1e
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-11-09 17:00:11 +0700

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by Ricky Primayuda Putra on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
